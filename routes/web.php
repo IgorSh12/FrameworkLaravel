53faff8a3692475d89b347587d08cece36632a0e
is42-shishoviya <igorbshihov@mail.ru>
2022-04-14 11:36:31 +0500

    Version 1.0.1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::middleware('SizeVerification')->group(function (){
+    Route::get('test', function (){
+        return 'Its okey';
+    });
+});
